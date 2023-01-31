bd6846ad01354529b1a32464f43df7f69a0b4ff3
Popov Max <drowpmv@gmail.com>
2023-01-31 14:38:36 +0100

    [006] add weapons with different params

--- packages/p006-weapons/src/plugins/weapon.rs
+++ packages/p006-weapons/src/plugins/weapon.rs
@@ -8,8 +8,9 @@ pub struct WeaponPlugin;
 
 impl Plugin for WeaponPlugin {
     fn build(&self, app: &mut App) {
-        app.insert_resource(LastFiredProjectile {
-            timer: Timer::new(Duration::from_millis(BULLETS_THROTTLE), TimerMode::Once),
+        app.insert_resource(CurrentWeapon {
+            weapon: Weapon::Pistol(PistolWeapon::new()),
+            fire_throttle: Timer::new(Duration::from_millis(0), TimerMode::Once),
         })
         .add_system(WeaponPlugin::fire_bullet);
     }
@@ -17,74 +18,230 @@ impl Plugin for WeaponPlugin {
 
 impl WeaponPlugin {
     fn fire_bullet(
-        mut commands: Commands,
+        commands: Commands,
         mouse_input: Res<Input<MouseButton>>,
         keyboard_input: Res<Input<KeyCode>>,
         player_query: Query<&Transform, With<Player>>,
         time: Res<Time>,
-        mut last_fired_projectile: ResMut<LastFiredProjectile>,
+        mut current_weapon: ResMut<CurrentWeapon>,
     ) {
-        last_fired_projectile.timer.tick(time.delta());
+        current_weapon.fire_throttle.tick(time.delta());
 
         if (mouse_input.pressed(MouseButton::Left) || keyboard_input.pressed(KeyCode::Space))
-            && last_fired_projectile.timer.finished()
+            && (current_weapon.fire_throttle.duration().is_zero()
+                || current_weapon.fire_throttle.finished())
         {
             let player_transform = player_query.single();
 
-            commands
-                .spawn(Bullet::from_player_position(player_transform))
-                .insert(Name::new("Bullet"));
+            match &current_weapon.weapon {
+                Weapon::Pistol(weapon) => {
+                    weapon.fire(commands, player_transform);
 
-            last_fired_projectile.timer.reset();
+                    let throttle_duration = weapon.bullet.throttle;
+                    current_weapon
+                        .fire_throttle
+                        .set_duration(Duration::from_millis(throttle_duration));
+                }
+                Weapon::Shotgun(weapon) => {
+                    weapon.fire(commands, player_transform);
+
+                    let throttle_duration = weapon.bullet.throttle;
+                    current_weapon
+                        .fire_throttle
+                        .set_duration(Duration::from_millis(throttle_duration));
+                }
+                Weapon::Rifle(weapon) => {
+                    weapon.fire(commands, player_transform);
+
+                    let throttle_duration = weapon.bullet.throttle;
+                    current_weapon
+                        .fire_throttle
+                        .set_duration(Duration::from_millis(throttle_duration));
+                }
+            }
+
+            current_weapon.fire_throttle.reset();
         }
     }
 }
 
-const BULLETS_THROTTLE: u64 = 200;
-
-#[derive(Resource, Debug)]
-struct LastFiredProjectile {
-    timer: Timer,
+#[derive(Resource)]
+struct CurrentWeapon {
+    weapon: Weapon,
+    fire_throttle: Timer,
 }
 
-const BULLET_SIZE: Vec2 = Vec2::new(2., 15.);
-const BULLET_COLOR: &str = "ffc48c";
-const BULLET_SPEED: f32 = 750.;
-const BULLET_DAMAGE: f32 = 20.;
+#[derive(Debug)]
+enum Weapon {
+    Pistol(PistolWeapon),
+    Shotgun(ShotgunWeapon),
+    Rifle(RifleWeapon),
+}
 
 #[derive(Bundle)]
-struct Bullet {
+struct FiredBullet {
     sprite: SpriteBundle,
     projectile: Projectile,
 }
 
-impl Bullet {
-    fn from_player_position(player_transform: &Transform) -> Self {
-        let position_correction = player_transform.rotation.mul_vec3(Vec3::Y * 20.);
+#[derive(Debug)]
+struct WeaponBullet {
+    size: Vec2,
+    color: &'static str,
+    speed: f32,
+    damage: f32,
+    throttle: u64,
+}
 
+#[derive(Debug)]
+struct PistolWeapon {
+    name: &'static str,
+    bullet: WeaponBullet,
+}
+
+impl PistolWeapon {
+    fn new() -> Self {
+        PistolWeapon {
+            name: "Pistol",
+            bullet: WeaponBullet {
+                size: Vec2::new(2., 15.),
+                color: "ffc48c",
+                speed: 750.,
+                damage: 20.,
+                throttle: 200,
+            },
+        }
+    }
+
+    fn fire(&self, mut commands: Commands, player_transform: &Transform) {
+        let position_correction = player_transform.rotation.mul_vec3(Vec3::Y * 20.);
         let transform = Transform {
             translation: player_transform.translation.clone() + position_correction,
-            scale: BULLET_SIZE.extend(1.0),
+            scale: self.bullet.size.extend(1.0),
             rotation: player_transform.rotation.clone(),
             ..default()
         };
 
-        Bullet {
-            sprite: SpriteBundle {
-                transform,
-                sprite: Sprite {
-                    color: Color::hex(BULLET_COLOR).expect("wrong bullet color"),
-                    custom_size: Some(Vec2::new(1.0, 1.0)),
+        commands
+            .spawn(FiredBullet {
+                sprite: SpriteBundle {
+                    transform,
+                    sprite: Sprite {
+                        color: Color::hex(self.bullet.color).expect("wrong bullet color"),
+                        custom_size: Some(Vec2::new(1.0, 1.0)),
+                        ..default()
+                    },
                     ..default()
                 },
-                ..default()
-            },
-            projectile: Projectile {
-                speed: BULLET_SPEED,
-                traveled: 0.,
-                size: BULLET_SIZE,
-                damage: BULLET_DAMAGE,
+                projectile: Projectile {
+                    speed: self.bullet.speed,
+                    traveled: 0.,
+                    size: self.bullet.size,
+                    damage: self.bullet.damage,
+                },
+            })
+            .insert(Name::new(self.name.to_owned() + &" bullet"));
+    }
+}
+
+#[derive(Debug)]
+struct ShotgunWeapon {
+    name: &'static str,
+    bullet: WeaponBullet,
+}
+
+impl ShotgunWeapon {
+    fn new() -> Self {
+        ShotgunWeapon {
+            name: "Shotgun",
+            bullet: WeaponBullet {
+                size: Vec2::new(6., 10.),
+                color: "ffffd1",
+                speed: 500.,
+                damage: 10.,
+                throttle: 1000,
             },
         }
+    }
+
+    fn fire(&self, mut commands: Commands, player_transform: &Transform) {
+        let position_correction = player_transform.rotation.mul_vec3(Vec3::Y * 20.);
+        let transform = Transform {
+            translation: player_transform.translation.clone() + position_correction,
+            scale: self.bullet.size.extend(1.0),
+            rotation: player_transform.rotation.clone(),
+            ..default()
+        };
+
+        commands
+            .spawn(FiredBullet {
+                sprite: SpriteBundle {
+                    transform,
+                    sprite: Sprite {
+                        color: Color::hex(self.bullet.color).expect("wrong bullet color"),
+                        custom_size: Some(Vec2::new(1.0, 1.0)),
+                        ..default()
+                    },
+                    ..default()
+                },
+                projectile: Projectile {
+                    speed: self.bullet.speed,
+                    traveled: 0.,
+                    size: self.bullet.size,
+                    damage: self.bullet.damage,
+                },
+            })
+            .insert(Name::new(self.name));
+    }
+}
+
+#[derive(Debug)]
+struct RifleWeapon {
+    name: &'static str,
+    bullet: WeaponBullet,
+}
+
+impl RifleWeapon {
+    fn new() -> Self {
+        RifleWeapon {
+            name: "Rifle",
+            bullet: WeaponBullet {
+                size: Vec2::new(2., 20.),
+                color: "aff8db",
+                speed: 1000.,
+                damage: 30.,
+                throttle: 100,
+            },
+        }
+    }
+
+    fn fire(&self, mut commands: Commands, player_transform: &Transform) {
+        let position_correction = player_transform.rotation.mul_vec3(Vec3::Y * 20.);
+        let transform = Transform {
+            translation: player_transform.translation.clone() + position_correction,
+            scale: self.bullet.size.extend(1.0),
+            rotation: player_transform.rotation.clone(),
+            ..default()
+        };
+
+        commands
+            .spawn(FiredBullet {
+                sprite: SpriteBundle {
+                    transform,
+                    sprite: Sprite {
+                        color: Color::hex(self.bullet.color).expect("wrong bullet color"),
+                        custom_size: Some(Vec2::new(1.0, 1.0)),
+                        ..default()
+                    },
+                    ..default()
+                },
+                projectile: Projectile {
+                    speed: self.bullet.speed,
+                    traveled: 0.,
+                    size: self.bullet.size,
+                    damage: self.bullet.damage,
+                },
+            })
+            .insert(Name::new(self.name));
     }
 }
